443e6041b270a7b18c3096bb6af8b76364bc9fe1
InvolvedTeacher <involvedteacher.dev@gmail.com>
2025-08-14 12:44:08 +0200

    feat (shift_ball): modifies ball to add shift functionality.

--- scripts/ball/ball.gd
+++ scripts/ball/ball.gd
@@ -3,14 +3,20 @@ class_name Ball
 
 var disabled: bool
 var player: CharacterBody2D
+var small_ball_bounce: float = 0.75
+var big_ball_bounce: float = 0.25
+var small: bool
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
+	small = true
 	disable_ball()
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("reset"):
 		disable_ball()
+	if Input.is_action_just_pressed("shift_ball"):
+		shift_ball()
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -18,16 +24,16 @@ func _physics_process(delta: float) -> void:
 	
 	velocity.x = move_toward(velocity.x, 0, delta)
 	
-	#move_and_slide()
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
 	if collision:
-		var reflect = collision.get_remainder().bounce(collision.get_normal())
-		velocity = velocity.bounce(collision.get_normal()) * 0.75
-		move_and_collide(reflect)
+		velocity = velocity.bounce(collision.get_normal()) * (
+				small_ball_bounce if small else big_ball_bounce)
 
 func disable_ball() -> void:
 	hide()
 	position = Vector2(-100, -100)
+	if not small:
+		shift_ball()
 	disabled = true
 
 # throw_ball throws the ball only if it is currently disabled
@@ -39,6 +45,15 @@ func throw_ball(strength: float) -> void:
 	velocity = strength * direction
 	show()
 	disabled = false
+
+func shift_ball() -> void:
+	if disabled:
+		return
+	small = not small
+	if small:
+		scale = Vector2(0.2, 0.2)
+	else:
+		scale = Vector2(1, 1)
 
 func get_disabled() -> bool:
 	return disabled
